f71a70bd7202e4292667e5fb71c793b71680f21c
Roxana <roxanacroitoru378@yahoo.ro>
2021-11-18 13:44:40 +0000

    respectarea principiului "do not repeat"

--- src/test/java/org/fasttrackit/SimpleSearchTest.java
+++ src/test/java/org/fasttrackit/SimpleSearchTest.java
@@ -22,9 +22,12 @@ public class SimpleSearchTest {
         //dechiderea unui browser//
         driver.get("http://testfasttrackit.info/selenium-test/");
 
+        //respectarea principiului do not repeat, principiul sigurantei este respectat
+        String searchKeyword = "camera";
+
         //interactionarea cu elementele din pagina,
         // cauta elementul: campul de cautare (search) si a introduce un text in el//
-        driver.findElement(By.id("search")).sendKeys("vase"+ Keys.ENTER);
+        driver.findElement(By.id("search")).sendKeys(searchKeyword + Keys.ENTER);
 
         System.out.println("Press Enter in search field.");
 
@@ -38,35 +41,7 @@ public class SimpleSearchTest {
 
         //cum identificam erori/bug-ri cu ajutorul testelor automate, test daca keyword vase se afla in toate produsele de pe pagina
         for(WebElement productName : productNames) {
-          assertThat("Some of the products`names do not contain the searched keyword.", productName.getText(), containsString("VASE"));
-        }
-    }
-
-    @Test
-    public void simpleSearchWithOneKeyword2 () {
-        System.setProperty("webdriver.chrome.driver", "C:\\Webdrivers\\chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
-
-        //dechiderea unui browser//
-        driver.get("http://testfasttrackit.info/selenium-test/");
-
-        //interactionarea cu elementele din pagina,
-        // cauta elementul: campul de cautare (search) si a introduce un text in el//
-        driver.findElement(By.id("search")).sendKeys("camera" + Keys.ENTER);
-
-        System.out.println("Press Enter in search field.");
-
-        //adaugarea unui produs in cos//
-        //driver.findElement(By.xpath("//div[@class = 'product-info' and ./descendant:: *[text() = 'Herald Glass Vase']]//button[@title='Add to Cart']")).click();
-
-        // stocarea elementelor intr-o lista, II mode h2.product-name > a//
-        List<WebElement> productNames = driver.findElements(By.cssSelector("h2.product-name a"));
-
-        System.out.println("Stored " + productNames.size() + " product names.");
-
-        //cum identificam erori/bug-ri cu ajutorul testelor automate, test daca keyword vase se afla in toate produsele de pe pagina
-        for (WebElement productName : productNames) {
-            assertThat("Some of the products`names do not contain the searched keyword.", productName.getText(), containsString("CAMERA"));
+          assertThat("Some of the products`names do not contain the searched keyword.", productName.getText(), containsString(searchKeyword.toUpperCase()));
         }
     }
 }
